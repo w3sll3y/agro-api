f2fb7b856ef00000ec775828f7e916123df7fe48
Wesley Fernandes <fernandess.weslley@gmail.com>
2024-12-19 21:45:20 -0300

    feat(auth): implement login generate jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,13 @@ import { UserModule } from 'src/user/user.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { LocalStrategy } from './strategies/local.strategy';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, JwtModule.register({
+    secret: process.env.JWT_SECRET,
+    signOptions: { expiresIn: '30d' },
+  })],
   controllers: [AuthController],
   providers: [AuthService, LocalStrategy]
 })
